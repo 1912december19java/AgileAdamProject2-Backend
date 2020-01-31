f2de132f8d040b4078de14f764400e49773adbef
Alex Mao <alexmao@Alexs-MacBook-Pro.local>
2020-01-31 11:57:27 -0500

    merged AssociateDao

--- project-2/src/main/java/project2/repository/AssociatesImp.java
+++ project-2/src/main/java/project2/repository/AssociatesImp.java
@@ -1,7 +1,9 @@
 package project2.repository;
 
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -30,6 +32,29 @@ public class AssociatesImp {
     
     return user;
     
+  }
+  
+  public List<Associates> getAllUsers(){
+    Session session = sf.getCurrentSession();
+    
+    Query q = session.createQuery("from Associates");
+    List<Associates> users = q.list();
+    
+    return users;
+  }
+  
+  public Associates getById(Integer id) {
+    Session session = sf.getCurrentSession();
+    
+    Associates user = (Associates) session.get(Associates.class, id);
+    
+    return user;    
+  }
+  
+  public void saveOrUpdate(Associates user) {
+    Session session = sf.getCurrentSession();
+    
+    session.saveOrUpdate(user);
   }
 
 }
