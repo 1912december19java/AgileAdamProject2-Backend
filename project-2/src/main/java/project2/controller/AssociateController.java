20e0501d49d4cd3a12cae7d04f8025c1814c4af7
unknown <mgisb003@hotmail.com>
2020-01-30 23:43:04 -0500

    servlet infrastructure complete

--- project-2/src/main/java/project2/controller/AssociateController.java
+++ project-2/src/main/java/project2/controller/AssociateController.java
@@ -1,6 +1,5 @@
 package project2.controller;
 
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
